8c7dc894aac4244ae62a5ab6223a0d958e72a2e9
boris-boat <84967370+boris-boat@users.noreply.github.com>
2023-01-11 23:49:47 +0100

    fixed create user bug

--- src/app/constants/url.constants.ts
+++ src/app/constants/url.constants.ts
@@ -1,4 +1,4 @@
 const BASE_URL = "https://podsetnik.herokuapp.com/kanban"
 export const SAVE_USER_URL = BASE_URL + "/updateUser"
 export const LOGIN_URL = BASE_URL + "/getuser"
-export const CREATE_USER_URL = BASE_URL + "/getuser"
+export const CREATE_USER_URL = BASE_URL + "/createUser"
